dd62985861562688065afde8c71a1376a2a33b50
Fabricio J. Silva <fabricio_fx@hotmail.com>
2017-07-11 19:11:46 -0300

    Adicionar arquivos de projeto.

---
+++ XamarinMVVM/XamarinMVVM/ViewModel/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinMVVM.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string nomePropriedade = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nomePropriedade));
+        }
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string nomePropriedade = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage,value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(nomePropriedade);
+            return true;
+        }
+    }
+}
---
+++ XamarinMVVM/XamarinMVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinMVVM.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _texto;
+
+        public string Texto
+        {
+            get { return _texto; }
+            set { SetProperty(ref _texto, value); }
+        }
+
+        public MainViewModel()
+        {
+            Texto = "Mostrar texto";
+        }
+
+    }
+}
